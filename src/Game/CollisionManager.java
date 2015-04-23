6c1d669d07f8177870da690d487bdce6ec155434
Nolan Sneed <dsneed@mines.edu>
2015-04-22 21:14:41 -0600

    Added Player and Enemy classes, as well as began writing CollisionManager.

--- src/Game/CollisionManager.java
+++ src/Game/CollisionManager.java
@@ -1,32 +1,35 @@
 package Game;
 
+import java.util.ArrayList;
+
 public class CollisionManager {
 	private boolean enemyHit = false; 
 	private boolean wallHit = false;
+	private ArrayList<Wall> walls;
+	private ArrayList<Target> targets;
+	private ArrayList<Enemy> enemies;
+	private Player player;
 	
-	
-	public boolean isWallHit() {
-		return false;
-	}
-	
-	public boolean isEnemyHit() {
-		return false;
-	}
-
 	public CollisionManager(){
 		
 	}
 	
+	public boolean isWallHit() {
+		return wallHit;
+	}
+	
+	public boolean isEnemyHit() {
+		return enemyHit;
+	}
 
-public void checkPlayerToEnemy() {
-	// TODO Auto-generated method stub
-	
-}
+	public void checkPlayerToEnemy() {
+		// TODO Auto-generated method stub
+		
+	}
 
-public void checkProjectileToWall(Projectile p) {
-	// TODO Auto-generated method stub
-	
-}
-	
-	
+	public void checkProjectileToWall(Projectile p) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
