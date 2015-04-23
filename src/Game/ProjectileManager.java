cd5f4fc14bf701920c47800e1a786f606543c44b
Patrick Sobolewski <psobolew@mymail.mines.edu>
2015-04-22 22:32:54 -0600

    Added basic functionality to projectile manager to allow for creation and deletion of shots.

--- src/Game/ProjectileManager.java
+++ src/Game/ProjectileManager.java
@@ -3,12 +3,46 @@ package Game;
 
 import java.util.ArrayList;
 
+import com.sun.javafx.geom.Vec2d;
+import com.sun.prism.Texture;
+
 public class ProjectileManager {
-	
+	private Texture texture;
+	private Vec2d position;
+	private Vec2d velocity;
+	private int speed;
+	private int rows;
+	private int columns;
+	private int FPS;
 	private ArrayList<Projectile> projectilePath = new ArrayList<Projectile>(); // ArrayList to hold all the positions of the projectile
+	private ArrayList<Projectile> projectiles;
 	
-	public ProjectileManager(){
+	public ProjectileManager(Texture texture, Vec2d initPosition, Vec2d velocity,
+			int speed, int rows, int columns, int FPS){
+		this.texture = texture;
+		this.position = initPosition;
+		this.velocity = velocity;
+		this.speed = speed;
+		this.rows = rows;
+		this.columns = columns;
+		this.FPS = FPS;
+	}
+	
+	public void Update(long elapsedTime) {
 		
+	}
+	
+	public void Draw() {
+		
+	}
+	
+	public void CreateProjectile() {
+		Projectile p = new Projectile(texture, position, velocity, speed, rows, columns, FPS);
+		projectiles.add(p);
+	}
+	
+	public void DeleteProjectile(int index) {
+		projectiles.remove(index);
 	}
 	
 	
